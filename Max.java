e77af2804539302b5bb514cc5c6610361ae0fd10
MehabubSyed77 <mehabubSyed9910@gmail.com>
2022-08-19 15:11:39 +0545

    initial commit

---
+++ Max.java
@@ -0,0 +1,12 @@
+public class Max{
+    public static void main(String[] args){
+        int A[]={12,23,43,23};
+        int num=Integer.MIN_VALUE;
+        for(int i=0; i<A.length; i++){
+            if(A[i]>num){
+                num=A[i];
+            }
+        }
+System.out.println(num);
+    }
+}
